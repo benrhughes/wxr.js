a455ed8feca5959dc5085385964208c5ebd69522
Ben Hughes <ben@benrhughes.com>
2011-09-11 02:25:12 +1000

    Moved export from Site to wxr

--- demo_wxr.js
+++ demo_wxr.js
@@ -1,14 +1,13 @@
 var wxr = require('./wxr');
 
 var site = new wxr.Site();
-
 site.title = "Ben's wordpress site";
 site.link = 'http://benswordpresssite.wordpress.com';
 site.description = 'Just another Wordpress blog';
 site.language = 'en';
 site.pubDate = 'Fri, 11 Jan 2008 20:23:05 +0000';
 
-var post = new wxr.Post();
+var post = new wxr.Post(site);
 post.title = 'About this site';
 post.name = 'about';
 post.link = site.link + '/' + post.name + '/';
@@ -27,6 +26,7 @@ post.tags.push('admin');
 
 var comment = new wxr.Comment(post);
 comment.id = 3;
+
 comment.author.name = 'Fred';
 comment.author.email = 'fred@fred.net';
 comment.author.url = 'http://fred.net';
@@ -37,10 +37,11 @@ comment.approved = 1;
 comment.date = '2008-05-16 22:49:10';
 comment.gmtDate = '2008-05-16 22:49:10'
 
+
 var att = new wxr.Attachment(post); 
 // extends Post, type === 'attachment'
 att.url = site.link + 'files/2010/08/stuff.jpg';
 att.metadata = 'metadata stuff';
 
 
-site.export('/path/to/file');
+wxr.export(site, '/path/to/file');
--- wxr.js
+++ wxr.js
@@ -2,3 +2,7 @@ exports.Site = require('./lib/site').Site;
 exports.Post = require('./lib/post').Post;
 exports.Comment = require('./lib/comment').Comment;
 exports.Attachment = require('./lib/attachment').Attachment;
+
+exports.export = function(site, filePath){
+	console.log('Exported to ' + filePath);
+};
