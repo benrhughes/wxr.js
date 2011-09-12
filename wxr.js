5bbd336178743f38922e02fcea955f6a39ff251b
Ben Hughes <ben@benrhughes.com>
2011-09-12 23:08:44 +1000

    Added xmlbuilder and started using it

--- wxr.js
+++ wxr.js
@@ -2,7 +2,9 @@ exports.Site = require('./lib/site').Site;
 exports.Post = require('./lib/post').Post;
 exports.Comment = require('./lib/comment').Comment;
 exports.Attachment = require('./lib/attachment').Attachment;
+exports.Category = require('./lib/category').Category;
+exports.Tag = require('./lib/category').Tag;
 
-exports.export = function(site, filePath){
-	console.log('Exported to ' + filePath);
+exports.export = function(site, file){
+	console.log('Exported to ' + file);
 };
---
+++ x.js
@@ -0,0 +1,13 @@
+var wxr = require('./wxr');
+
+var site = new wxr.Site();
+site.title = "Ben's blog";
+site.description = "This is Ben's blog";
+
+var cat = new wxr.Category();
+cat.name = "Stuff I Love";
+cat.niceName = "stuff";
+
+site.categories.push(cat);
+
+console.log(site.toWXR());
