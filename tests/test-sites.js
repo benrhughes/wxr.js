fb703f2efcca77f81cca4ba1e5caa8b348c4cc0d
Ben Hughes <ben@benrhughes.com>
2011-09-29 23:07:58 +1000

    More Site tests

--- tests/test-sites.js
+++ tests/test-sites.js
@@ -85,3 +85,71 @@ test('Description node created', function(){
 	value = node.children[0];
 	value.should.have.property('value', "<![CDATA[Ben\'s awesome site]]>");
 });
+
+test('Language node created', function(){
+	var site = new wxr.Site();
+	
+	var doc = site.toWXR();
+	var node = _(doc.children[1].children)
+					.detect(function(c){ return c.name == 'language';});
+
+	should.exist(node);
+
+	value = node.children[0];
+	value.should.have.property('value', 'en'); 
+});
+
+test('Generator node created', function(){
+	var site = new wxr.Site();
+	
+	var doc = site.toWXR();
+	var node = _(doc.children[1].children)
+					.detect(function(c){ return c.name == 'generator';});
+
+	should.exist(node);
+
+	value = node.children[0];
+	value.should.have.property('value', 'http://github.com/benrhughes/wrx'); 
+});
+
+test('PubDate node created', function(){
+	var site = new wxr.Site();
+	site.pubDate = Date.now();	
+	
+	var doc = site.toWXR();
+	var node = _(doc.children[1].children)
+					.detect(function(c){ return c.name == 'pubDate';});
+
+	should.exist(node);
+
+	value = node.children[0];
+	value.should.have.property('value', site.pubDate.toString()); 
+});
+
+test('Base site URL node created', function(){
+	var site = new wxr.Site();
+	site.baseSiteURL = 'http://mysite.com';	
+	
+	var doc = site.toWXR();
+	var node = _(doc.children[1].children)
+					.detect(function(c){ return c.name == 'base_site_url';});
+
+	should.exist(node);
+
+	value = node.children[0];
+	value.should.have.property('value', site.baseSiteURL); 
+});
+
+test('Base blog URL node created', function(){
+	var site = new wxr.Site();
+	site.baseSiteURL = 'http://mysite.com/blog';	
+	
+	var doc = site.toWXR();
+	var node = _(doc.children[1].children)
+					.detect(function(c){ return c.name == 'base_blog_url';});
+
+	should.exist(node);
+
+	value = node.children[0];
+	value.should.have.property('value', site.baseBlogURL); 
+});
